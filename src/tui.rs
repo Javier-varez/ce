d4d54306bcd6ad328de4b2dc815f07c6fa0dac81
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-07-02 20:54:46 +0200

    Add stdout and stderr

--- src/tui.rs
+++ src/tui.rs
@@ -12,6 +12,14 @@ pub fn update<B: Backend>(
 ) -> Result<(), std::io::Error> {
     terminal
         .draw(|f| {
+            let parts = tui::layout::Layout::default()
+                .direction(tui::layout::Direction::Vertical)
+                .constraints([
+                    tui::layout::Constraint::Percentage(33),
+                    tui::layout::Constraint::Percentage(33),
+                    tui::layout::Constraint::Percentage(33),
+                ])
+                .split(f.size());
             let mut text = vec![];
             for asm in &compilation.asm {
                 text.push(Spans::from(format!("{}", asm.text)));
@@ -24,7 +32,35 @@ pub fn update<B: Backend>(
             ));
 
             let paragraph = Paragraph::new(text).block(block).wrap(Wrap { trim: true });
-            f.render_widget(paragraph, f.size());
+            f.render_widget(paragraph, parts[0]);
+
+            let mut text = vec![];
+            for stdout in &compilation.stdout {
+                text.push(Spans::from(format!("{}", stdout.text)));
+            }
+            let block = Block::default().borders(Borders::ALL).title(Span::styled(
+                "Stdout",
+                Style::default()
+                    .fg(Color::Magenta)
+                    .add_modifier(Modifier::BOLD),
+            ));
+
+            let paragraph = Paragraph::new(text).block(block).wrap(Wrap { trim: true });
+            f.render_widget(paragraph, parts[1]);
+
+            let mut text = vec![];
+            for stderr in &compilation.stderr {
+                text.push(Spans::from(format!("{}", stderr.text)));
+            }
+            let block = Block::default().borders(Borders::ALL).title(Span::styled(
+                "Stderr",
+                Style::default()
+                    .fg(Color::Magenta)
+                    .add_modifier(Modifier::BOLD),
+            ));
+
+            let paragraph = Paragraph::new(text).block(block).wrap(Wrap { trim: true });
+            f.render_widget(paragraph, parts[2]);
         })
         .unwrap();
     Ok(())
